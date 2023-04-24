c871007e61dc959b161a032f233821f4f0ceb61e
Justus Helfrich <justushelfrich@Justus-Helfrich.local>
2023-04-24 11:22:45 +0200

    shortend the basic code to make it faster and easier to read

--- termpapercode.R
+++ termpapercode.R
@@ -33,50 +33,6 @@ new_set <- working_set %>% group_by(NUTS_ID) %>%
 table(new_set$n_rest)
 upperb = 500
 
-# creating the regression data set by adding dummy variables
-brdata <- new_set %>% 
-  mutate(nrest = as.factor(ifelse(n_rest <= upperb, n_rest,upperb))) %>% 
-  dplyr::select(! n_rest)
-
-brdata <- dummy_cols(brdata, select_columns="nrest")
-
-brdata2 <- brdata
-varn <- names(brdata %>% dplyr::select(starts_with("nrest_")))
-
-# filling the rows with 1 until the true value 
-for (i in 1:nrow(brdata2)) {
-  row_i <- as.matrix(brdata2[i, varn])
-  first_one_col <- match(1, row_i)  # index of the first 1 in the row
-  if (!is.na(first_one_col)) {
-    brdata2[i, (varn[1:(first_one_col - 1)])] <- 1
-  }
-}
-
-# Adding variables to the data ####
-# get cuisines as Boolean variable (We could use this as variables to explain the number of restaurants in a region)
-ger_info_cuisine <- ger_allinfo2 %>% 
-  dplyr::select(restaurant_link, NUTS_ID, cuisines) %>% 
-  separate_rows(cuisines, sep = ", ") %>%
-  mutate(new_col = 1) %>%
-  pivot_wider(names_from = cuisines, values_from = new_col, values_fill = 0)
-
-ger_info_cuisine_nut3 <- ger_info_cuisine %>% 
-  dplyr::select(! restaurant_link) %>% 
-  group_by(NUTS_ID) %>% summarise_all(sum)
-
-# get info_tag as Boolean variable
-ger_info_tag <- ger_allinfo2 %>% dplyr::select(restaurant_link, NUTS_ID, top_tags) %>% 
-  separate_rows(top_tags, sep = ", ") %>%
-  mutate(new_col = 1) %>%
-  pivot_wider(names_from = top_tags, values_from = new_col, values_fill = 0) %>%
-  mutate_at(c("Cheap Eats"), as.numeric) %>%
-  rename(cheap_eats = "Cheap Eats")
-
-# extract the info about cheap food
-cheap_eats <- ger_info_tag %>% 
-  group_by(NUTS_ID) %>% 
-  summarise(cheap = sum(cheap_eats))
-
 # Pre-Work for Regression ####
 working_set$n_rest <- working_set$n_rest %>% as.numeric()
 regression_set <- working_set %>% 
@@ -87,11 +43,6 @@ regression_set <- working_set %>%
             COAST_TYPE = mean(COAST_TYPE),
             n_rest = mean(n_rest),
             area = mean(area))
-
-regression_set <- regression_set %>% 
-  merge(., ger_info_cuisine_nut3, by = "NUTS_ID") %>%
-  merge(., cheap_eats, by = "NUTS_ID") 
-
 
 # create a vector of breaks for grouping
 breaks <- c(0, 50, 70, 90, 110, 130, 140, 165, 190, 220, 250, 300, 450, Inf)
@@ -145,3 +96,57 @@ knitr::kable(ETR_N, col.names = c("ETR"), digits = 4,
              caption = 'Entry threshold ratios',
              booktabs = TRUE)
 
+
+
+# advanced modelling ####
+# Adding variables to the data 
+# get cuisines as Boolean variable (We could use this as variables to explain the number of restaurants in a region)
+ger_info_cuisine <- ger_allinfo2 %>% 
+        dplyr::select(restaurant_link, NUTS_ID, cuisines) %>% 
+        separate_rows(cuisines, sep = ", ") %>%
+        mutate(new_col = 1) %>%
+        pivot_wider(names_from = cuisines, values_from = new_col, values_fill = 0)
+
+ger_info_cuisine_nut3 <- ger_info_cuisine %>% 
+        dplyr::select(! restaurant_link) %>% 
+        group_by(NUTS_ID) %>% summarise_all(sum)
+
+# get info_tag as Boolean variable
+ger_info_tag <- ger_allinfo2 %>% dplyr::select(restaurant_link, NUTS_ID, top_tags) %>% 
+        separate_rows(top_tags, sep = ", ") %>%
+        mutate(new_col = 1) %>%
+        pivot_wider(names_from = top_tags, values_from = new_col, values_fill = 0) %>%
+        mutate_at(c("Cheap Eats"), as.numeric) %>%
+        rename(cheap_eats = "Cheap Eats")
+
+# extract the info about cheap food
+cheap_eats <- ger_info_tag %>% 
+        group_by(NUTS_ID) %>% 
+        summarise(cheap = sum(cheap_eats))
+regression_set <- regression_set %>% 
+        merge(., ger_info_cuisine_nut3, by = "NUTS_ID") %>%
+        merge(., cheap_eats, by = "NUTS_ID") 
+
+
+
+
+
+#Code from the lab that is actually not needed
+# # creating the regression data set by adding dummy variables
+# brdata <- new_set %>% 
+#   mutate(nrest = as.factor(ifelse(n_rest <= upperb, n_rest,upperb))) %>% 
+#   dplyr::select(! n_rest)
+# 
+# brdata <- dummy_cols(brdata, select_columns="nrest")
+# 
+# brdata2 <- brdata
+# varn <- names(brdata %>% dplyr::select(starts_with("nrest_")))
+# 
+# # filling the rows with 1 until the true value 
+# for (i in 1:nrow(brdata2)) {
+#   row_i <- as.matrix(brdata2[i, varn])
+#   first_one_col <- match(1, row_i)  # index of the first 1 in the row
+#   if (!is.na(first_one_col)) {
+#     brdata2[i, (varn[1:(first_one_col - 1)])] <- 1
+#   }
+# }
