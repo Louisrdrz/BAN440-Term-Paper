da5bf5fdda79a7dac5a2c3dd1fe53a849d11a73e
Justus Helfrich <justushelfrich@Justus-Helfrich.local>
2023-04-24 16:08:22 +0200

    clean up and added second model for italian restaurants being explained by cheap name tag.

--- termpapercode.R
+++ termpapercode.R
@@ -29,20 +29,11 @@ working_set <- ger_allinfo2
 touristic_df <- read_csv("tourist_nut2.csv")
 toursitic_21 <- touristic_df %>% subset(TIME_PERIOD == 2021) %>% subset(., grepl("^DE", geo)) %>% dplyr::select(geo, OBS_VALUE)
 tnew <- toursitic_21[nchar(toursitic_21$geo) == 4, ]
-min <- tnew[order(tnew$OBS_VALUE), ][1:15, ]
+min <- tnew[order(tnew$OBS_VALUE), ][1:20, ]
 smallest_values <- subset(tnew, OBS_VALUE %in% min$OBS_VALUE)
 tourismfilter <- as.list(unique(smallest_values$geo))
 
 working_set <- ger_allinfo2[grepl(paste(tourismfilter, collapse = "|"), ger_allinfo2$NUTS_ID),]
-
-
-
-# summarizing the data on the NUT3 level 
-new_set <- working_set %>% group_by(NUTS_ID) %>%
-  summarise(n_rest = mean(n_rest))
-
-table(new_set$n_rest)
-upperb = 500
 
 # Pre-Work for Regression ####
 working_set$n_rest <- working_set$n_rest %>% as.numeric()
@@ -54,7 +45,7 @@ regression_set <- working_set %>%
             COAST_TYPE = mean(COAST_TYPE),
             n_rest = mean(n_rest),
             area = mean(area)) %>% 
-        subset(area < 750 & Population < 750)
+        subset(area < 700 & Population < 500)
 
 # create a vector of breaks for grouping
 breaks <- c(0, 50, 70, 90, 110, 130, 140, 165, 190, 220, 250, 300, 450, Inf)
@@ -72,6 +63,7 @@ aggregate(area ~ group, data = regression_set, FUN = mean)
 # Regression ####
 regression_set$n_rest <- regression_set$n_rest %>%
         as.factor()
+
 model1 <- polr(n_rest ~ MOUNT_TYPE + URBN_TYPE + COAST_TYPE + area, data = regression_set, method = "probit")
 summary(model1)
 
@@ -129,7 +121,7 @@ ger_info_tag <- ger_allinfo2 %>% dplyr::select(restaurant_link, NUTS_ID, top_tag
         pivot_wider(names_from = top_tags, values_from = new_col, values_fill = 0) %>%
         mutate_at(c("Cheap Eats"), as.numeric) %>%
         rename(cheap_eats = "Cheap Eats")
-
+table(ger_info_cuisine_nut3$Italian)
 # extract the info about cheap food
 cheap_eats <- ger_info_tag %>% 
         group_by(NUTS_ID) %>% 
@@ -137,8 +129,58 @@ cheap_eats <- ger_info_tag %>%
 regression_set <- regression_set %>% 
         merge(., ger_info_cuisine_nut3, by = "NUTS_ID") %>%
         merge(., cheap_eats, by = "NUTS_ID") 
+table(regression_set$Italian)
+# create a vector of breaks for grouping
+breaks <- c(0, 10, 20, 30, 40, Inf)
+
+# create a vector of labels for the groups
+labels <- c("0-10", "11-20", "21-30", "31-40", "40+")
+
+regression_set$Italian <- as.numeric(regression_set$Italian)
+# use cut function to group the n_rest column
+regression_set$groupI <- cut(regression_set$Italian, breaks = breaks, labels = labels)
+
+regression_set$Italian <- as.factor(regression_set$Italian)
+model3 <- polr(Italian ~ log(Population) + cheap, data = regression_set, method = "probit")
+summary(model3)
+
+
+upperb <- 40 ## Number of sections, can be modified
+
+lambda<-model3$coefficients # Estimates
+theta<-model3$zeta # Cutoffs
+
+S_N <- exp(theta - mean(regression_set$cheap)*lambda[2])
+
+slab<-NULL
+for (i in 1:(upperb)) {slab[i]<-paste0("$S_",i,"$")}
+names(S_N)<-slab
+
+ETR_N <- exp(theta[2:upperb] - theta[2:upperb-1]) * (1:(upperb-1))/(2:upperb)
+
+elab<-NULL
+for (i in 2:upperb) {elab[i-1]<-paste0("$s_",i,"/s_",i-1,"$")}
+names(ETR_N)<-elab
+
+
+knitr::kable(S_N, col.names = c("'000s"), digits = 4,
+             caption = 'Entry thresholds',
+             booktabs = TRUE)
+
+knitr::kable(ETR_N, col.names = c("ETR"), digits = 4,
+             caption = 'Entry threshold ratios',
+             booktabs = TRUE)
+
+
+
 
 #Code from the lab that is actually not needed
+# summarizing the data on the NUT3 level 
+# new_set <- working_set %>% group_by(NUTS_ID) %>%
+#         summarise(n_rest = mean(n_rest))
+# 
+# table(new_set$n_rest)
+# upperb = 500
 # # creating the regression data set by adding dummy variables
 # brdata <- new_set %>% 
 #   mutate(nrest = as.factor(ifelse(n_rest <= upperb, n_rest,upperb))) %>% 
